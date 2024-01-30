c4bfb6b3de334dc9e7c31921af9095468258e2d8
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-01-30 14:30:05 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 149 ms, Memory: 21,764 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= 10; tc++) {
+			// get input
+			int dumpCount = Integer.parseInt(br.readLine());
+			List<Integer> list = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			while (st.hasMoreTokens()) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+
+			// process
+			Collections.sort(list);
+			for (int i = 0; i < dumpCount; i++) {
+				if (solve(list)) {
+					break;
+				}
+			}
+
+			sb.append('#').append(tc).append(' ').append(list.get(list.size() - 1) - list.get(0)).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	private static boolean solve(List<Integer> list) {
+		int min = list.get(0);
+		int max = list.get(list.size() - 1);
+
+		if (max - min <= 1) {
+			return true;
+		}
+
+		min++;
+		max--;
+
+		list.remove(0);
+		list.remove(list.size() - 1);
+
+		int idx = 0;
+		while (list.get(idx) < min) {
+			idx++;
+		}
+		list.add(idx, min);
+
+		idx = list.size() - 1;
+		while (list.get(idx) > max) {
+			idx--;
+		}
+		list.add(idx + 1, max);
+		
+		return false;
+	}
+
+}
