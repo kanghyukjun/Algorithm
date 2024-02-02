655196468fbd467229b04cc4871c5180a4266a86
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-02-02 17:40:35 +0900

    [Gold IV] Title: 친구비, Time: 164 ms, Memory: 16992 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -30,8 +29,7 @@ public class Main {
 		}
 		return true;
 	}
-
-	// 최소 비용을 가진 노드가 가장 최상위 노드가 되도록 구현
+    
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
