5a5150e371319e6734d79e799aab5643804327b8
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-02-02 17:39:43 +0900

    [Gold IV] Title: 친구비, Time: 172 ms, Memory: 17108 KB -BaekjoonHub

---
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] money;
+	static int[] parent;
+
+	public static int find(int n1) {
+		if (n1 == parent[n1]) {
+			return n1;
+		}
+		return parent[n1] = find(parent[n1]);
+	}
+
+	public static boolean union(int n1, int n2) {
+		int r1 = find(n1);
+		int r2 = find(n2);
+		if (r1 == r2) {
+			return false;
+		}
+
+		if (money[r1] < money[r2]) {
+			parent[r2] = r1;
+		} else {
+			parent[r1] = r2;
+		}
+		return true;
+	}
+
+	// 최소 비용을 가진 노드가 가장 최상위 노드가 되도록 구현
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		// get input
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		money = new int[N + 1];
+		parent = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			money[i] = Integer.parseInt(st.nextToken());
+			parent[i] = i;
+		}
+
+		// process
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			union(n1, n2);
+		}
+		
+		int minSum = 0;
+		for (int i = 1; i <= N; i++) {
+			if (i == parent[i]) {
+				minSum += money[i];
+			}
+		}
+
+		if (minSum <= k) {
+			System.out.println(minSum);
+		} else {
+			System.out.println("Oh no");
+		}
+	}
+
+}
