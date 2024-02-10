7f78ca5a12e8bf3d993fc22fb59c205f2254cd09
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-02-11 00:05:13 +0900

    [D3] Title: 격자판 칠하기, Time: 108 ms, Memory: 18,796 KB -BaekjoonHub

--- SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.java
+++ SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.java
@@ -1,14 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int[] drow = { 1, -1, 0, 0 };
-	static int[] dcol = { 0, 0, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,7 +17,6 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
 
-			Queue<int[]> que = new ArrayDeque<>();
 			char[][] arr = new char[N][M];
 			for (int i = 0; i < N; i++) {
 				String input = br.readLine();
@@ -32,7 +26,7 @@ public class Solution {
 			}
 
 			// process
-			boolean flag = true;
+			boolean flag;
 			if (arr[0][0] == '#') {
 				flag = find(arr, '#');
 			} else if (arr[0][0] == '.') {
