d1d5617b3829de4eeb33a4df191b32c9e1788873
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-02-21 10:32:25 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,3 +1,5 @@
+// https://buyandpray.tistory.com/73 참고
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,37 +8,64 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	static int[] arr;
+	static int[] dp;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 
 		// get input
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		arr = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
 		// process
-		int[] dp = new int[N];
-		Arrays.fill(dp, 1);
-		int output = 1;
-		for (int i = N - 1; i >= 0; i--) {
-			int max = 0;
-			for (int j = i + 1; j < N; j++) {
-				if (arr[i] < arr[j]) {
-					max = Math.max(max, dp[j]);
-				}
+		dp = new int[N + 1];
+		int back = 1;
+		for (int i = 1; i <= N; i++) {
+			// 이진 탐색을 이용해 새롭게 삽입을 할 위치를 찾기
+			int idx = binarySearch(0, back, arr[i]);
+
+			// 찾았으면 해당 위치를 업데이트
+			if (idx != -1) {
+				dp[idx] = arr[i];
 			}
-			if(max >= 1) {
-				dp[i] = max + 1;
+
+			// 그렇지 않다면 back에 해당 인덱스를 추가하고 back을 1 증가시키기
+			else {
+				dp[back] = arr[i];
+				back++;
 			}
-			output = Math.max(dp[i], output);
 		}
 
 		// output
-		System.out.println(output);
+//		System.out.println(Arrays.toString(dp));
+		System.out.println(back - 1);
+	}
+
+	private static int binarySearch(int left, int right, int find) {
+		int mid = (left + right) / 2;
+
+		if (left > right) {
+			return -1;
+		}
+
+		else if (mid  + 1 < dp.length && dp[mid] < find && find <= dp[mid + 1]) {
+			return mid + 1;
+		}
+
+		else if (dp[mid] < find) {
+			return binarySearch(mid + 1, right, find);
+		}
+
+		else {
+			return binarySearch(left, mid - 1, find);
+		}
+
 	}
 
 }
