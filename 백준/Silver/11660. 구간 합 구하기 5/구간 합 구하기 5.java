dd7819fc89d9eff4f41ae384701f36b5ea390889
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-03-03 10:07:14 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 752 ms, Memory: 132028 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
 
 		// get input
 		st = new StringTokenizer(br.readLine());
@@ -15,29 +16,36 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[N + 1][N + 1];
-		int[][] sum = new int[N + 1][N + 1];
-
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				sum[i][j] = arr[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
 			}
 		}
 
 		// process
-		StringBuilder sb = new StringBuilder();
+		int[][] dp = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+			}
+		}
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-
-			sb.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]).append('\n');
+			sb.append(solve(dp, x1, y1, x2, y2)).append('\n');
 		}
-		
+
+		// output
 		System.out.println(sb);
+	}
+
+	private static int solve(int[][] arr, int x1, int y1, int x2, int y2) {
+		return arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1];
 	}
 
 }
