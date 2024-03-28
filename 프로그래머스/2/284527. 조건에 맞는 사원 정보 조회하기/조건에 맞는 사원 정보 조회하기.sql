eec7334ede4743c54a7d3c6d4249d3417747afad
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-03-28 16:44:24 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,20 +1,12 @@
-
 # 1. 점수 합을 구한다
 # 2. 그 중 최대 점수를 구한다
 
 select MS.score, MS.emp_no, EMP.emp_name, EMP.position, EMP.email
 from hr_employees EMP inner join (
-                            select sum(score) as score, emp_no
+                            select emp_no, sum(score) as score
                             from hr_grade
                             group by emp_no
-                            having sum(score) = (
-                                select max(A.score)
-                                from (
-                                    select emp_no, sum(score) as score
-                                    from hr_grade
-                                    group by emp_no
-                                ) A
-                            )
+                            order by score desc
+                            limit 1
                         ) MS
-                        on EMP.emp_no = MS.emp_no
-
+                        using(emp_no)
