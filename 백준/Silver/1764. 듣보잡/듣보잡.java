7c1c883666034f5d9f79a9516b3ade18d4a426be
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-04-16 22:26:27 +0900

    [Silver IV] Title: 듣보잡, Time: 260 ms, Memory: 31080 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+
+	static class Node {
+		boolean isWord;
+		Node[] childs;
+
+		public Node() {
+			isWord = false;
+			childs = new Node['z' - 'a' + 1];
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+
+		// get input
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		Node root = new Node();
+		for (int i = 0; i < n; i++) {
+			String line = br.readLine();
+			Node head = root;
+			for (int j = 0; j < line.length(); j++) {
+				char c = line.charAt(j);
+				if (head.childs[c - 'a'] == null) {
+					head.childs[c - 'a'] = new Node();
+				}
+				head = head.childs[c - 'a'];
+			}
+			head.isWord = true;
+		}
+
+		// process
+		int count = 0;
+		TreeSet<String> set = new TreeSet<>();
+		for (int i = 0; i < m; i++) {
+			String line = br.readLine();
+			Node head = root;
+			boolean check = true;
+			for (int j = 0; j < line.length(); j++) {
+				char c = line.charAt(j);
+				if (head.childs[c - 'a'] == null) {
+					check = false;
+					break;
+				}
+				head = head.childs[c - 'a'];
+			}
+			if (head.isWord && check) {
+				count++;
+				set.add(line);
+			}
+		}
+		sb.append(count).append('\n');
+		while (!set.isEmpty())
+			sb.append(set.pollFirst()).append('\n');
+		System.out.println(sb);
+	}
+
+}
