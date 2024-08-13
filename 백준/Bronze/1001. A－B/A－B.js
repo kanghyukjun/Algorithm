88b0850e079bb7851e3bfa10003aa862757583ac
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-08-13 18:36:16 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const [N, M] = fs.readFileSync(filePath, "utf-8").trim().split(" ");
+console.log(N - M);
