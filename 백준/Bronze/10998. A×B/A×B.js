60c16a048cac7337e9e95f17d06bf3541287e5df
Hyukjun Kang <105738124+kanghyukjun@users.noreply.github.com>
2024-10-09 21:40:29 +0900

    [Bronze V] Title: A×B, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./util/input.txt";
+let [N, M] = fs.readFileSync(filePath, "utf-8").trim().split(" ").map(Number);
+console.log(N*M);
